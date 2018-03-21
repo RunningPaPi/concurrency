abded6e991f58b368e4726b2bf19c4a842313dac
RunningPaPi <1196923457@qq.com>
2018-03-22 01:59:31 +0800

    注释
    删除注释

--- concurrency/src/main/java/com/mmall/concurrency/controller/TestController.java
+++ concurrency/src/main/java/com/mmall/concurrency/controller/TestController.java
@@ -4,11 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
-/**
- * 
- * @author mjf
- *
- */
+
 @RestController
 @Slf4j
 public class TestController {
