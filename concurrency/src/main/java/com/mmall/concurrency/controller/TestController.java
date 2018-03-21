28827b1aa98e5ea446a3f7b74d4609d0dddcb764
mjf <mjf@mjf-PC>
2018-03-22 01:54:52 +0800

    添加注释

--- concurrency/src/main/java/com/mmall/concurrency/controller/TestController.java
+++ concurrency/src/main/java/com/mmall/concurrency/controller/TestController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
-
+/**
+ * 
+ * @author mjf
+ *
+ */
 @RestController
 @Slf4j
 public class TestController {
